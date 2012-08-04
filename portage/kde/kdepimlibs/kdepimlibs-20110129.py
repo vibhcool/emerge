5708fd692aa08018fc810b9d28d9bce8b53556a2
Andre Heinecke <aheinecke@intevation.de>
2012-08-04 14:39:16 +0000

    Merge from enterprise

--- portage/kde/kdepimlibs/kdepimlibs-20110129.py
+++ portage/kde/kdepimlibs/kdepimlibs-20110129.py
@@ -3,14 +3,17 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['gitHEAD'] = '[git]kde:kdepimlibs'
+        for version in ['4.4', '4.5', '4.6', '4.7', '4.8', '4.9']:
+            self.svnTargets[version] = '[git]kde:kdepimlibs|KDE/%s' % version
         self.defaultTarget = 'gitHEAD'
 
     def setDependencies( self ):
         self.dependencies['kde/kdelibs'] = 'default'
         self.dependencies['kdesupport/akonadi'] = 'default'
-        self.dependencies['win32libs-bin/cyrus-sasl'] = 'default'
-        self.dependencies['win32libs-bin/libical'] = 'default'
-        self.dependencies['win32libs-bin/gpgme'] = 'default'
+        self.dependencies['win32libs-sources/cyrus-sasl-src'] = 'default'
+        self.dependencies['win32libs-sources/boost-src'] = 'default'
+        self.dependencies['win32libs-sources/libical-src'] = 'default'
+        self.dependencies['enterprise5/gpg4win-e5'] = 'default'
         self.dependencies['win32libs-bin/openldap'] = 'default'
         self.shortDescription = "the base libraries for PIM related services"
 
@@ -20,9 +23,6 @@ class Package(CMakePackageBase):
     def __init__( self ):
         self.subinfo = subinfo()
         CMakePackageBase.__init__( self )
-        self.boost = portage.getPackageInstance('win32libs-bin','boost')
-        path = self.boost.installDir()
-        os.putenv( "BOOST_ROOT", path )
 
 if __name__ == '__main__':
     Package().execute()
